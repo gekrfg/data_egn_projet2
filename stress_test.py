1c0ab6bbe41545d793a31179e27b13ff1333271d
gekrfg <runpeng.ye@efrei.net>
2020-12-15 19:11:56 +0800

    add requirements and test

---
+++ stress_test.py
@@ -0,0 +1,33 @@
+import unittest
+import requests
+import time
+import os
+
+
+class FlaskTests(unittest.TestCase):
+    def setUp(self):
+        os.environ['NO_PROXY'] = '0.0.0.0'
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_index(self):
+        n = 1000
+        responses_urls = []
+
+        for i in range(n):
+            responses_urls.append(requests.get('http://localhost:5000'))
+
+        start = time.time()
+        end = time.time()
+        t = end - start
+
+        for i in range(n):
+            self.assertEqual(responses_urls[i].status_code, 200)
+
+        print("The {} requests took: {} seconds".format(n, t))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test.py
@@ -0,0 +1,42 @@
+from webapp import string_similar, get_similar_tweets
+import unittest
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
+class FlaskTests(unittest.TestCase):
+
+    def setUp(self):
+        os.environ['NO_PROXY'] = '0.0.0.0'
+        self.text = {
+            'sentence': "I love you!",
+        }
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_a_webpage(self):
+        response = requests.get('http://localhost:5000')
+        self.assertEqual(response.status_code, 200)
+
+    def test_b_send(self):
+        params = {
+            'sentence': self.text['sentence'],
+            "form_type": "analysis_sentence"
+        }
+        response = requests.post('http://localhost:5000', data=params)
+        self.assertEqual(response.status_code, 200)
+
+    def test_c_send(self):
+        params = {
+            'sentence': self.text['sentence'],
+            "form_type": "analysis_sentence"
+        }
+        response = requests.post('http://localhost:5000', data=params)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
